4bc40bd9f3ed729f8905922da79f15a37c9dcac8
ex47 <anatoliy.yemets@gmail.com>
2016-09-03 04:29:32 +0300

    autocomplete field adapter autocomplete placeholder json retrieval using volley WIP

--- app/src/main/java/ua/edu/zu/zsutimetable/MainActivity.java
+++ app/src/main/java/ua/edu/zu/zsutimetable/MainActivity.java
@@ -17,12 +17,17 @@ import android.view.MenuItem;
 
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
+
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
@@ -33,6 +38,8 @@ public class MainActivity extends AppCompatActivity
 
     String facultyParam;
     int currFacultyPos;
+
+
     Response.Listener<String> listener = new Response.Listener<String>() {
         @Override
         public void onResponse(String response) {
@@ -41,6 +48,8 @@ public class MainActivity extends AppCompatActivity
             Toast.makeText(context, response, Toast.LENGTH_SHORT).show();
         }
     };
+
+
     Response.ErrorListener errorListener = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
@@ -51,6 +60,7 @@ public class MainActivity extends AppCompatActivity
             toast.show();
         }
     };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,29 +103,58 @@ public class MainActivity extends AppCompatActivity
 
         navigationView.setNavigationItemSelectedListener(this);
 
-        // TODO: add reaction to click
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-        spinner.setOnItemSelectedListener(spinnerListener);
+        Spinner facultySpinner = (Spinner) findViewById(R.id.spinner);
+        facultySpinner.setOnItemSelectedListener(spinnerListener);
 
         String[] values = this.getResources().getStringArray(R.array.faculty_values);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, values);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
+        ArrayAdapter<String> facutySpinnerAdapter = new ArrayAdapter<>(context, android.R.layout.simple_spinner_item, values);
+        facutySpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        facultySpinner.setAdapter(facutySpinnerAdapter);
 
         EditText editText = (EditText) findViewById(R.id.editText);
 
 
         // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://dekanat.zu.edu.ua/cgi-bin/timetable.cgi?n=701&lev=141&query=%D0%9A";
+        String url = "http://dekanat.zu.edu.ua/cgi-bin/timetable.cgi?faculty=1001&teacher=&group=63_%B3_%E4&sdate=05.09.2016&edate=&n=700";
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, listener, errorListener);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, listener, errorListener);
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        //queue.add(stringRequest);
 
         editText.setText("" + currFacultyPos);
+
+        AutoCompleteTextView groupField = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        groupField.setAdapter(new PlacesAutoCompleteAdapter(this, R.layout.list_item));
+
+        String urlJSON = "http://dekanat.zu.edu.ua/cgi-bin/timetable.cgi?n=701&lev=142&faculty=1001&query=6";
+
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Context context = getApplicationContext();
+                        Toast.makeText(context, response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+
+                    }
+                });
+
+
+        //TODO: FIX SINGLETON
+        // Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsObjRequest);
+
+
 
         //String[] both = Stream.concat(Arrays.stream(a), Arrays.stream(b)).toArray(String[]::new);
 
@@ -123,6 +162,12 @@ public class MainActivity extends AppCompatActivity
 
 }
          */
+    }
+
+    private ArrayList<String> autocomplete(String input) {
+        ArrayList<String> resultList = null;
+
+        return resultList;
     }
 
     @Override
@@ -180,5 +225,52 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class PlacesAutoCompleteAdapter extends ArrayAdapter<String> implements Filterable {
+        private ArrayList<String> resultList;
+
+        public PlacesAutoCompleteAdapter(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+        }
+
+        @Override
+        public int getCount() {
+            return resultList.size();
+        }
+
+        @Override
+        public String getItem(int index) {
+            return resultList.get(index);
+        }
+
+        @Override
+        public Filter getFilter() {
+            Filter filter = new Filter() {
+                @Override
+                protected FilterResults performFiltering(CharSequence constraint) {
+                    FilterResults filterResults = new FilterResults();
+                    if (constraint != null) {
+                        // Retrieve the autocomplete results.
+                        resultList = autocomplete(constraint.toString());
+
+                        // Assign the data to the FilterResults
+                        filterResults.values = resultList;
+                        filterResults.count = resultList.size();
+                    }
+                    return filterResults;
+                }
+
+                @Override
+                protected void publishResults(CharSequence constraint, FilterResults results) {
+                    if (results != null && results.count > 0) {
+                        notifyDataSetChanged();
+                    } else {
+                        notifyDataSetInvalidated();
+                    }
+                }
+            };
+            return filter;
+        }
     }
 }
